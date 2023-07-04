866b58afb45aa372b0a3ffed9ce1aa1034ded987
Sandeep Kunichi <138591182+Sandeepkun@users.noreply.github.com>
2023-07-04 10:22:56 -0400

    Create stringManipulation.js

---
+++ stringManipulation.js
@@ -0,0 +1,6 @@
+const string = "Hello, world!";
+
+console.log(`Original String: ${string}`);
+console.log(`Upper Case: ${string.toUpperCase()}`);
+console.log(`Lower Case: ${string.toLowerCase()}`);
+console.log(`Length of string: ${string.length}`);
