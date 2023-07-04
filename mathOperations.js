a1357679858a9b6df2f42e1070639989b276efa4
Sandeep Kunichi <138591182+Sandeepkun@users.noreply.github.com>
2023-07-04 10:22:41 -0400

    Create mathOperations.js

---
+++ mathOperations.js
@@ -0,0 +1,9 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+console.log(`Add: ${add(5, 2)}`);
+console.log(`Subtract: ${subtract(5, 2)}`);
+console.log(`Multiply: ${multiply(5, 2)}`);
+console.log(`Divide: ${divide(5, 2)}`);
